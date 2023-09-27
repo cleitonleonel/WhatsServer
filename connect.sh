8662845133dbb87625c90a73edc2ee12f1a7316d
cleiton <cleiton.leonel@gmail.com>
2023-09-27 11:42:09 -0300

    Alteração no readme e inclusão de exemplos de chamada dos endpoints via curl.

--- connect.sh
+++ connect.sh
@@ -1,21 +1,3 @@
-## _WhatsServer_
-
-<img src="https://github.com/cleitonleonel/WhatsServer/blob/master/whats.png?raw=true" alt="whatsserver" width="200"/>
-
-Server básico para pequenos projetos de automação do whatsapp usando [whatsapp-web.js](https://wwebjs.dev/),
-para fins educativos.
-
-### Instalação e uso
-
-```shell
-git clone https://github.com/cleitonleonel/WhatsServer.git
-cd WhatsServer
-sudo chmod +x ./install
-./install
-node app.js
-```
-
-```shell
 # Verificando se a sessão existe
 curl -X POST "http://localhost:3333/api/v1/check" \
     -H "User-Agent: Mozilla/5.0 (MSIE; Windows 10)" \
@@ -48,16 +30,3 @@ curl -X POST "http://localhost:3333/api/v1/upload" \
     -F "caption=*Cleiton* Creton" \
     -F "number=5527995772291" \
     -F "file=@/home/cleiton/backup.ico"
-```
-
-### Este projeto ajudou você?
-
-Se esse projeto ajudar de alguma forma, sinta-se livre para me pagar um café, kkkk...Basta apontar a câmera do seu celular para um dos qrcodes abaixo.
-
-<img src="https://github.com/cleitonleonel/pypix/blob/master/qrcode.png?raw=true" alt="QRCode Doação" width="250"/>
-
-<img src="https://github.com/cleitonleonel/pypix/blob/master/artistic.gif?raw=true" alt="QRCode Doação" width="250"/>
-
-### Author
-
-Cleiton Leonel Creton ==> cleiton.leonel@gmail.com
