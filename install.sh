2e768e4dd383ddb48aa0ab17ae5c4acb5ff57e63
cleiton <cleiton.leonel@gmail.com>
2023-10-04 17:45:23 -0300

    alterações no script de instalação.

--- install.sh
+++ install.sh
@@ -16,10 +16,18 @@ libappindicator1 libnss3 lsb-release \
 xdg-utils wget build-essential \
 apt-transport-https libgbm-dev
 
+node_version="-- lts"
 base_dir=$(pwd)
 base_name=$(basename "${base_dir}")
 username=$1
 password=$2
+
+os_version=$(lsb_release -rs)
+. /etc/os-release
+
+if (( $(echo "$os_version < 20.04" | bc -l) )); then
+  node_version="16.15.1"
+fi
 
 echo "OLHA O BASENAME: ${base_name}"
 
@@ -55,6 +63,8 @@ if [ "$base_name" != "WhatsServer" ]; then
   git clone https://github.com/cleitonleonel/WhatsServer.git
 fi
 
+
+
 wget https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh -O nvm_install.sh
 chmod  +x ./nvm_install.sh
 ./nvm_install.sh
@@ -67,12 +77,34 @@ export NVM_DIR="$HOME/.nvm"
 EOF
 
 su - "$username" -c 'bash -l -c "echo USUÁRIO: $USER && echo PATH: $PWD && source ~/.bashrc"'
-su - "$username" -c 'bash -l -c "source ~/.nvm/nvm.sh && nvm install --lts"'
+su - "$username" -c 'bash -l -c "source ~/.nvm/nvm.sh && nvm install $node_version"'
+su - "$username" -c 'bash -l -c "echo \\"$(node -v | sed 's/v//')\\" > ~/WhatsServer/.nvmrc"'
 su - "$username" -c 'bash -l -c "source ~/.nvm/nvm.sh && nvm install-latest_npm"'
 su - "$username" -c 'bash -l -c "source ~/.nvm/nvm.sh && nvm use node"'
 su - "$username" -c 'bash -l -c "source ~/.nvm/nvm.sh && npm -v"'
 su - "$username" -c 'bash -l -c "source ~/.nvm/nvm.sh && npm install -g npm@latest"'
 su - "$username" -c 'bash -l -c "source ~/.nvm/nvm.sh && cd WhatsServer && npm install"'
+
+cd "$username"
+
+service_content="[Unit]
+Description=WhatsServer instance to serve $username
+After=network.target
+
+[Service]
+User=$username
+Group=$username
+WorkingDirectory=/home/whatsserver/WhatsServer
+ExecStart=/bin/bash -lc \'source ~/.nvm/nvm.sh && nvm use $(cat .nvmrc) && node app.js\'
+
+[Install]
+WantedBy=multi-user.target"
+
+service_file="/etc/systemd/system/${username}.service"
+echo "$service_content" | sudo tee "$service_file" > /dev/null
+
+sudo systemctl daemon-reload
+sudo systemctl enable "$username"
 
 sudo chown "$username:$username" -R "/home/$username"
 sudo chmod -R 755 "/home/$username"
